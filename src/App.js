1dd4c02ed367d2b4afec9ee67c3020c8ae1ce985
Chethan <you@example.com>
2024-04-30 18:07:33 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,32 @@
+import React from 'react'
+
+const Wroggles = () => {
+  return (
+    <div style={{width:'100vw',height:'100vh',display:'flex',alignItems:'center',justifyContent:'center'}}>
+<div style={{width:'100vw',height:70,display:'flex',alignItems:'center' ,justifyContent:'space-around',position:'absolute',top:0,backgroundColor:'black'}}>
+  <span style={{color:'white'}}>home</span>
+  <span style={{color:'white'}}>orders</span>
+  <span style={{color:'white'}}>cart</span>
+  <span style={{color:'white'}}>profile</span>
+
+  
+</div>
+
+
+<div style={{width:70,height:'100vh',backgroundColor:'black',position:'absolute',left:0.}}>
+
+  
+
+</div>
+<div style={{width:70,height:'100vh',backgroundColor:'black',position:'absolute',right:0}}>
+
+</div>
+
+<div style={{height:'50vh',width:'50vw',backgroundColor:'yellow' ,display:'flex',justifyContent:'center',alignItems:'center'}}>
+  <h1 style={{color:'red'}}>welcome to web devoloper</h1>
+   </div>
+    </div>
+  )
+}
+
+export default Wroggles
